c6caa45061dd0b2b70b80970ad226bca00cfc8ab
Janine Mueller <janine2.mueller@tu-dortmund.de>
2017-06-08 15:57:35 +0200

    Deleted obsolete stuff

--- WindingControl.h
+++ WindingControl.h
@@ -13,16 +13,15 @@
 #include <string>
 
 //OpenCV stuff
-#include <opencv2/highgui/highgui.hpp>  // GUI windows and trackbars
-#include <opencv2/video/video.hpp>  // camera/video classes, imports background_segm
-#include <opencv2/calib3d/calib3d.hpp>  // for FindHomography
-#include <opencv2/features2d/features2d.hpp>  // for FeatureDetection
-#include <opencv2/videostab/stabilizer.hpp>
-#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 //mvIMPACT stuff
 #include <apps/Common/exampleHelper.h>
+#include <common/minmax.h>
 #include <mvIMPACT_CPP/mvIMPACT_acquire.h>
+
 
 using namespace std;
 using namespace cv;
@@ -38,23 +37,5 @@ void SaveSingleImageToFile(Device* pDev, string path);
 // sets an chosen Output to an On or Off state
 void SetOutput( Device* pDev, int Output, bool On );
 
-
-
-
-
-// OBSOLETE //
-void ProzessFrame(Device* pDev, bool stab);
-void ProzessCapturedFrame(string VideoName, bool stab);
-void stabilizeFrame(Mat& whole, Mat& stabframe, string method);
-void stabilize(string method, const vector<Point2f>& feat_prev, const vector<Point2f>& feat_curr, Mat& whole);
-void stabilize_OpticalFlow(const vector<Point2f>& feat_prev, const vector<Point2f>& feat_curr, Mat& whole);
-void stabilize_RigidTransform(const vector<Point2f>& feat_prev, const vector<Point2f>& feat_curr, Mat& whole);
-void stabilize_Homography(const vector<Point2f>& feat_prev, const vector<Point2f>& feat_curr, Mat& whole);
-void stabilize_Videostab(const vector<Point2f>& feat_prev, const vector<Point2f>& feat_curr, Mat& whole);
-void clearBadFeatures(vector<Point2f>& feat_prev, vector<Point2f>& feat_curr, vector<uchar>& stat, vector<float>& err);
-vector<KeyPoint> findFeatures(Mat& img, string method, vector<Point2f>& feat);
-void drawArrows(Mat& drawframe, const vector<Point2f>& feat_1, const vector<Point2f>& feat_2);
-void drawPoints(Mat& drawframe, const vector<Point2f>& feat, Scalar s);
-Point2f getOpticalFlow(const vector<Point2f>& feat_prev, const vector<Point2f>& feat_curr);
 
 #endif /* defined(____WindingControl__) */
--- WindingControlMain.cpp
+++ WindingControlMain.cpp
@@ -13,7 +13,9 @@
 
 //mvIMPACT stuff
 #include <apps/Common/exampleHelper.h>
+#include <common/minmax.h>
 #include <mvIMPACT_CPP/mvIMPACT_acquire.h>
+
 
 using namespace std;
 
@@ -25,76 +27,76 @@ int main()
 
 	Device* pDev = OpenCam(devMgr);
 
-	cout << "Do you want to record a video? [y/n]" << endl;
-	cin >> input;
+	// cout << "Do you want to record a video? [y/n]" << endl;
+	// cin >> input;
 
-	if((input == "y") | (input == "Yes") | (input == "yes") | (input == "Y")){
+	// if((input == "y") | (input == "Yes") | (input == "yes") | (input == "Y")){
 
-		time_t sec = time(NULL);
+	// 	time_t sec = time(NULL);
 
-		tm *uhr = localtime(&sec);
+	// 	tm *uhr = localtime(&sec);
 
-		stringstream path;
+	// 	stringstream path;
 		
 		
-		path << "/local/Wickel_" << uhr->tm_year-100 << uhr->tm_mon+1 << uhr->tm_mday << "-" << uhr->tm_hour << uhr->tm_min << uhr->tm_sec << ".avi";
+	// 	path << "/local/Wickel_" << uhr->tm_year-100 << uhr->tm_mon+1 << uhr->tm_mday << "-" << uhr->tm_hour << uhr->tm_min << uhr->tm_sec << ".avi";
 
 
 
-		WriteVideoToFile(path.str(), pDev);
+	// 	WriteVideoToFile(path.str(), pDev);
 
-	}
+	// }
 
-	if((input == "n") | (input == "No") | (input == "no") | (input == "N")){
+	// if((input == "n") | (input == "No") | (input == "no") | (input == "N")){
 
-		cout << "Do you want to take a snapshot?" << endl;
-		cin >> input;
-		if((input == "y") | (input == "Yes") | (input == "yes") | (input == "Y")){
+	// 	cout << "Do you want to take a snapshot?" << endl;
+	// 	cin >> input;
+	// 	if((input == "y") | (input == "Yes") | (input == "yes") | (input == "Y")){
 
-			Mat frame;
-			stringstream path;
+	// 		Mat frame;
+	// 		stringstream path;
 
-			time_t sec = time(NULL);
+	// 		time_t sec = time(NULL);
 
-			tm *uhr = localtime(&sec);		
+	// 		tm *uhr = localtime(&sec);		
 
-			path << "/home/e5a-labor/pic/Wickel_" << uhr->tm_year-100 << uhr->tm_mon+1 << uhr->tm_mday << "-" << uhr->tm_hour << uhr->tm_min << uhr->tm_sec << ".png";
+	// 		path << "/home/e5a-labor/pic/Wickel_" << uhr->tm_year-100 << uhr->tm_mon+1 << uhr->tm_mday << "-" << uhr->tm_hour << uhr->tm_min << uhr->tm_sec << ".png";
 
-			cout << path.str() << endl;
-
-
-			frame = ImageRequestSingle(pDev);
-
-			cout << "frame.size(): " << frame.size() << "mat.type(): " << frame.type() << endl;
-
-			/*vector<int> compression_params;
-			compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-			compression_params.push_back(9);*/
-
-			try {
-				imwrite(path.str(), frame);
-				cout << "saved to " << path.str() << endl;
-			}
-			catch (runtime_error& ex) {
-				fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
-			}
-
-		}
-
-		else{
-
-			cout << "Camera picture will be just shown..." << endl;
-			ShowPictureOfCamera(pDev);
-
-		}
+	// 		cout << path.str() << endl;
 
 
-	}
+	// 		frame = ImageRequestSingle(pDev);
+
+	// 		cout << "frame.size(): " << frame.size() << "mat.type(): " << frame.type() << endl;
+
+	// 		/*vector<int> compression_params;
+	// 		compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	// 		compression_params.push_back(9);*/
+
+	// 		try {
+	// 			imwrite(path.str(), frame);
+	// 			cout << "saved to " << path.str() << endl;
+	// 		}
+	// 		catch (runtime_error& ex) {
+	// 			fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
+	// 		}
+
+	// 	}
+
+	// 	else{
+
+	// 		cout << "Camera picture will be just shown..." << endl;
+	// 		ShowPictureOfCamera(pDev);
+
+	// 	}
+
+
+	// }
 	
-	else{
-		cout << "Camera picture will be just shown..." << endl;
-		ShowPictureOfCamera(pDev);
-	}
+	// else{
+	// 	cout << "Camera picture will be just shown..." << endl;
+	// 	ShowPictureOfCamera(pDev);
+	// }
 	
 
 	return 0;
